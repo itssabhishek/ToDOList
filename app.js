da20e17d835e2d0477dd2e17c2f0d1ff5bdf3c1a
Abhishek0710 <you@example.com>
2021-02-06 22:37:06 +0530

    4th Commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
@@ -11,21 +12,12 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
+ 
 app.use(express.static("public"));
 
-app.get("/", function(req, res) {
+app.get("/", function(_req, res) {
 
-    let today = new Date();
-
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-
-    };
-
-    let day = today.toLocaleDateString("en-US", options);
+let day = date.getDate();
 
     res.render("list", {
         listTitle: day,
@@ -48,14 +40,14 @@ app.post("/", function(req, res) {
 
 });
 
-app.get("/Work", function(req, res) {
+app.get("/Work", function(_req, _res) {
     app.render("list", {
         listTitle: "Work List",
         newListItems: workItems
     });
 });
 
-app.get("/about", function (req , res) {
+app.get("/about", function (_req , res) {
     res.render("about");
 });
 
@@ -65,6 +57,6 @@ app.post("/Work", function(req, res) {
     res.redirect("/Work");
 });
 
-app.listen(3000, function() {
-    console.log("server is running on port 3000");
+app.listen(8000, function() {
+    console.log("server is running on port 8000");
 });
